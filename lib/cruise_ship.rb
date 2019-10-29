d60abbdb0bee9d5553ebbec03451fb2a271defe9
Luis Herrera <herrera.lrh@gmail.com>
2019-10-29 01:13:36 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,18 +1,17 @@
-passengers = {
- suite_a: "Amanda Presley",
- suite_b: "Seymour Hoffman",
- suite_c: "Alfred Tennyson",
- suite_d: "Charlie Chaplin",
- suite_e: "Crumpet the Elf"
- }
+#passengers = {
+#suite_a: "Amanda Presley",
+#suite_b: "Seymour Hoffman",
+#suite_c: "Alfred Tennyson",
+#suite_d: "Charlie Chaplin",
+#suite_e: "Crumpet the Elf"
+#}
 
-def select_winner(passengers)
+def select_winner(hash)
   winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
-
-winner
+  hash.each {|k,v|
+    if k == :suite_a && v.start_with?("A")
+      winner = v
+    end
+  }
+  winner
 end
