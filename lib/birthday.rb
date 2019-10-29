d901edd74c7af7100cb6699d2ece3e9616765026
Luis Herrera <herrera.lrh@gmail.com>
2019-10-29 01:11:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,9 @@
-# birthday_kids = {
-#   "Timmy" => 9,
-#   "Sarah" => 6,
-#   "Amanda" => 27
-# }
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-   end
+def select_winner(hash)
+  winner = ""
+  hash.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
 end
